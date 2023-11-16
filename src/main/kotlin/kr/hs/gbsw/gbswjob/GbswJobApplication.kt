90c206556f9380158f3eb3e0ca538caefc711a43
wxodi0 <teayang06@gmail.com>
2023-11-16 16:48:16 +0900

    Initial commit

---
+++ src/main/kotlin/kr/hs/gbsw/gbswjob/GbswJobApplication.kt
@@ -0,0 +1,11 @@
+package kr.hs.gbsw.gbswjob
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GbswJobApplication
+
+fun main(args: Array<String>) {
+    runApplication<GbswJobApplication>(*args)
+}
---
+++ src/test/kotlin/kr/hs/gbsw/gbswjob/GbswJobApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.hs.gbsw.gbswjob
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GbswJobApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
