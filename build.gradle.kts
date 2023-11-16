0fe7b4ca95565f491efebfb12e5e5742808187ce
wxodi0 <teayang06@gmail.com>
2023-11-16 21:40:21 +0900

    Changes by kimdaeyong

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,14 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.security:spring-security-core")
+
+    implementation("org.springframework.security:spring-security-jwt:1.1.1.RELEASE")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
