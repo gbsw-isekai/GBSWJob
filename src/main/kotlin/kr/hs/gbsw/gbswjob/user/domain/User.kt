8ca293b18913fa10294a1609a37e573861ecd1ba
wxodi0 <teayang06@gmail.com>
2023-12-01 00:00:10 +0900

    Add roles in User

--- src/main/kotlin/kr/hs/gbsw/gbswjob/user/domain/User.kt
+++ src/main/kotlin/kr/hs/gbsw/gbswjob/user/domain/User.kt
@@ -2,6 +2,9 @@ package kr.hs.gbsw.gbswjob.user.domain
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.ManyToMany
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 
 @Entity
 class User(
