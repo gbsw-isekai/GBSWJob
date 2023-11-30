93411ca7237ae4bfcfa449176e380bc40681c074
wxodi0 <teayang06@gmail.com>
2023-12-01 00:01:44 +0900

    Add roles in User

--- src/main/kotlin/kr/hs/gbsw/gbswjob/user/domain/User.kt
+++ src/main/kotlin/kr/hs/gbsw/gbswjob/user/domain/User.kt
@@ -8,7 +8,16 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority
 
 @Entity
 class User(
-        @Id var id: String,
+        @Id
+        var id: String,
         var pw: String,
+
+        @ManyToMany
+        var roles: List<Role>
 ) {
+        fun generateGrantedAuthorities(): List<GrantedAuthority> {
+                return roles.map {
+                        SimpleGrantedAuthority(it.id)
+                }
+        }
 }
