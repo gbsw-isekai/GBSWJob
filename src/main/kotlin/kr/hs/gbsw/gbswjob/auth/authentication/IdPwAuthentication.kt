cbf5610dd8d7de1c45fb3aa7ca1e78e3672308e5
wxodi0 <teayang06@gmail.com>
2023-12-01 00:04:04 +0900

    "Authentication에 User roles 추가"

--- src/main/kotlin/kr/hs/gbsw/gbswjob/auth/authentication/IdPwAuthentication.kt
+++ src/main/kotlin/kr/hs/gbsw/gbswjob/auth/authentication/IdPwAuthentication.kt
@@ -1,6 +1,17 @@
 package kr.hs.gbsw.gbswjob.auth.authentication
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.GrantedAuthority
 
-class IdPwAuthentication(principal: Any?, credentials: Any?) : UsernamePasswordAuthenticationToken(principal, credentials) {
+class IdPwAuthentication(principal: Any?, credentials: Any?, authorities: List<GrantedAuthority>) : UsernamePasswordAuthenticationToken(principal, credentials, authorities) {
+
 }
+
+//class IdPwAuthentication extedns UserName.... {
+//    Object principal;
+//    Object credentials;
+//
+//    IdPwAuthentication(prinicpal, crednetnials) {
+//        super(principal, crede, authorities)
+//    }
+//}
