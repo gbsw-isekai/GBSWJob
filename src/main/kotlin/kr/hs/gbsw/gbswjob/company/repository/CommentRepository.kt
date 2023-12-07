cda5e079ed0423269023018ff9e7f81ddfd166a3
wxodi0 <teayang06@gmail.com>
2023-12-08 03:01:42 +0900

    fix: 띄어쓰기 ><

--- src/main/kotlin/kr/hs/gbsw/gbswjob/company/repository/CommentRepository.kt
+++ src/main/kotlin/kr/hs/gbsw/gbswjob/company/repository/CommentRepository.kt
@@ -3,5 +3,5 @@ package kr.hs.gbsw.gbswjob.company.repository
 import kr.hs.gbsw.gbswjob.company.domain.Comment
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CommentRepository : JpaRepository<Comment,Int>{
+interface CommentRepository : JpaRepository<Comment, Int>{
 }
