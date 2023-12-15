bf17dfc01891acd0e81674cb7cd1971e99224d35
wxodi0 <teayang06@gmail.com>
2023-12-15 14:24:42 +0900

    feat: rename companyView

--- src/main/kotlin/kr/hs/gbsw/gbswjob/company/repository/CompanyViewsRepository.kt
+++ src/main/kotlin/kr/hs/gbsw/gbswjob/company/repository/CompanyViewsRepository.kt
@@ -1,8 +1,8 @@
 package kr.hs.gbsw.gbswjob.company.repository
 
-import kr.hs.gbsw.gbswjob.company.domain.CompanyViews
+import kr.hs.gbsw.gbswjob.company.domain.CompanyView
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CompanyViewsRepository : JpaRepository<CompanyViews, Int> {
+interface CompanyViewsRepository : JpaRepository<CompanyView, Int> {
 
 }
