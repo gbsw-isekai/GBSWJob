e5bf0d33f1efc3e429b0eaf2f0c0c312694ad4a1
wxodi0 <teayang06@gmail.com>
2024-01-02 12:09:45 +0900

    feat: 회사 entity 데이터 수정

--- src/main/kotlin/kr/hs/gbsw/gbswjob/company/domain/Company.kt
+++ src/main/kotlin/kr/hs/gbsw/gbswjob/company/domain/Company.kt
@@ -1,5 +1,6 @@
 package kr.hs.gbsw.gbswjob.company.domain
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -10,28 +11,34 @@ import jakarta.persistence.OneToMany
 class Company(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Int?,
-    var name: String,
-    var stack: String,
-    var averageSalary: Int,
+    var id: Int, //회사
+    var name: String, //회사 명
+    var postalCode: String, //우편번호
+    var address: String, //주소
+    var industryCode: Int, //업종코드
+    var industry: String, //업종명
+    var registrationNumber: Long, //사업자 등록번호
+    var viewCount: Long,
+
+    @OneToMany(mappedBy = "company")
+    var companyNpsEmployeeData: MutableList<CompanyNpsEmployeeData>?,
     @OneToMany(mappedBy = "company")
     var comments: MutableList<CompanyComment>?,
     @OneToMany(mappedBy = "company")
     var reviews: MutableList<CompanyReview>?,
     @JsonIgnore
     @OneToMany(mappedBy = "company")
-    var views: MutableList<CompanyView>?,
-    var viewCount: Long
+    var views: MutableList<CompanyView>?
 ) {
-    fun calculateAverageRating(): Double {
-        if (reviews.isNullOrEmpty()) {
-            return 0.0
-        }
-
-        val ratingSum = reviews?.sumOf {
-            it.calculateAverageRating()
-        } ?: 0.0
-
-        return ratingSum / reviews!!.size
-    }
+//    fun calculateAverageRating(): Double {
+//        if (reviews.isNullOrEmpty()) {
+//            return 0.0
+//        }
+//
+//        val ratingSum = reviews?.sumOf {
+//            it.calculateAverageRating()
+//        } ?: 0.0
+//
+//        return ratingSum / reviews!!.size
+//    }
 }
