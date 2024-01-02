cf63abe5e83690f214ee5324982738ba17ee58d0
wxodi0 <teayang06@gmail.com>
2024-01-02 14:39:07 +0900

    feat: USER NULL 허용

--- src/main/kotlin/kr/hs/gbsw/gbswjob/auth/controller/AuthController.kt
+++ src/main/kotlin/kr/hs/gbsw/gbswjob/auth/controller/AuthController.kt
@@ -4,6 +4,7 @@ import kr.hs.gbsw.gbswjob.auth.authentication.dto.LoginDto
 import kr.hs.gbsw.gbswjob.auth.service.AuthService
 import kr.hs.gbsw.gbswjob.common.AuthUserId
 import kr.hs.gbsw.gbswjob.user.dto.UserRegisterDto
+import kr.hs.gbsw.gbswjob.user.repository.UserRepository
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/auth")
 class AuthController(
-        private val service: AuthService
+    private val service: AuthService,
+    private val repository: UserRepository,
 ) {
 
     // 회원 가입
@@ -21,7 +23,7 @@ class AuthController(
         @RequestBody dto: UserRegisterDto,
         @AuthUserId userId: String?
     ): String {
-        return service.register(dto, userId!!)
+        return service.register(dto, userId)
     }
     @PostMapping("/login")
     fun login(@RequestBody dto: LoginDto): String {
