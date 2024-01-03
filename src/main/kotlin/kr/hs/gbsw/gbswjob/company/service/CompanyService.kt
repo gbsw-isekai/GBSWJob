c17ff2b322c88fa182da09e2a0c564110374c758
wxodi0 <teayang06@gmail.com>
2024-01-03 17:24:19 +0900

    feat: getCompanies api 메서드 수정 및 paging 추가

--- src/main/kotlin/kr/hs/gbsw/gbswjob/company/service/CompanyService.kt
+++ src/main/kotlin/kr/hs/gbsw/gbswjob/company/service/CompanyService.kt
@@ -3,12 +3,19 @@ package kr.hs.gbsw.gbswjob.company.service
 import kr.hs.gbsw.gbswjob.company.domain.Company
 import kr.hs.gbsw.gbswjob.company.domain.CompanyView
 import kr.hs.gbsw.gbswjob.company.dto.CompanyGetDto
+import kr.hs.gbsw.gbswjob.company.dto.CompanyListGetDto
 import kr.hs.gbsw.gbswjob.company.repository.CompanyNpsEmployeeDataRepository
 import kr.hs.gbsw.gbswjob.company.repository.CompanyRepository
 import kr.hs.gbsw.gbswjob.company.repository.CompanyViewsRepository
 import kr.hs.gbsw.gbswjob.user.repository.UserRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
 import kotlin.IllegalArgumentException
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
 @Service
 class CompanyService(
@@ -33,23 +40,36 @@ class CompanyService(
 //
 //        return companyRepository.save(company)
 //    }
-    fun getCompanies(pageId: Int): Page<CompanyListGetDto> {
-
+    fun getCompanies(pageId: Int, searchId: String?): Page<CompanyListGetDto> {
         val pageable = PageRequest.of(pageId, 10, Sort.by(Sort.Direction.DESC, "viewCount"))
-        val company = companyRepository.findAll(pageable).map {
-            CompanyListGetDto(
-                it.id,
-                it.name,
-                it.postalCode,
-                it.address,
-                it.industryCode,
-                it.industry,
-                it.registrationNumber,
-                it.viewCount
-//                it.companyNpsEmployeeData
-            )
+
+        if(searchId == null) {
+            return companyRepository.findAll(pageable).map {
+                CompanyListGetDto(
+                    it.id,
+                    it.name,
+                    it.postalCode,
+                    it.address,
+                    it.industryCode,
+                    it.industry,
+                    it.registrationNumber,
+                    it.viewCount
+                )
+            }
+        } else {
+            return companyRepository.findAllByName(pageable, searchId).map {
+                CompanyListGetDto(
+                    it.id,
+                    it.name,
+                    it.postalCode,
+                    it.address,
+                    it.industryCode,
+                    it.industry,
+                    it.registrationNumber,
+                    it.viewCount
+                )
+            }
         }
-        return company;
     }
 
     fun getCompany(companyId: Int): CompanyGetDto {
@@ -59,7 +79,7 @@ class CompanyService(
 
         var averageYearPrice = 0L;
 
-        val companyNps = companyNpsEmployeeDataRepository.findAllByCompanyId(companyId).map {
+        companyNpsEmployeeDataRepository.findAllByCompanyId(companyId).map {
             averageYearPrice = (it.monthlyPrice.toInt() / 0.09 / it.total.toInt()).roundToLong()
         }
 
